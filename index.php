a51634d54fa6e1aa9d8be8e9fa00a35bc4f6df6d
CaianSantana <caian23@outlook.com>
2024-01-17 11:26:09 -0300

    form inicial

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+
+    <div class="container">
+    
+        <div class="card">
+            <div class="card-header">
+                LOGIN
+            </div>
+        </div>
+        <div class="card-body">
+            <form action="" method="POST">
+
+                <input type ="text" nome="usuario" placeholder="Usuario" class="form-control">
+                <input type ="password" nome="senha" placeholder="Senha" class="form-control">
+
+                <button type="submit" class="btn btn-primary">ENTRAR</button>
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
